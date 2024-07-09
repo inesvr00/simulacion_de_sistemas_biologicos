44b416234b7fb69a284f9f440c5183a222891f61
Inés VR <inesvictoria2000pro@gmail.com>
2024-07-09 21:02:04 +0100

    improving chain decay

--- src/exercises/chain_decay_model.py
+++ src/exercises/chain_decay_model.py
@@ -17,11 +17,15 @@ class RadioactiveDecayChainSimulation:
         self.N_p = self.N_p0
         self.N_h = 0
         self.lambda_p, self.lambda_h = decay_constants
+        
+        # Initial state of nuclei: 2 for parent, 1 for first daughter, 0 for second daughter
         self.state = np.ones(num_nuclei, dtype=int) * 2
         self.time = 0
+        
+        # Histories for plotting and analysis
         self.time_history = [self.time]
         self.remaining_nuclei_history = [self.num_nuclei]
-        self.state_distribution_history = [[np.sum(self.state == i) for i in range(1, 5)]]
+        self.state_distribution_history = [[np.sum(self.state == i) for i in range(0, 3)]]
         self.theoretical_N_p_history = [self.N_p]
         self.theoretical_N_h_history = [self.N_h]
 
@@ -42,9 +46,11 @@ class RadioactiveDecayChainSimulation:
         self.time += dt  
         self.time_history.append(self.time)
 
+        # Update histories
         self.remaining_nuclei_history.append(np.count_nonzero(self.state > 0))
         self.state_distribution_history.append([np.sum(self.state == i) for i in range(0, 3)])
         
+        # Update theoretical histories
         N_p_t, N_h_t = theoretical_solution(self.N_p0, self.N_h0, self.lambda_p, self.lambda_h, self.time)
         self.theoretical_N_p_history.append(N_p_t)
         self.theoretical_N_h_history.append(N_h_t)
@@ -59,26 +65,25 @@ class RadioactiveDecayChainSimulation:
         plt.ylabel('Remaining Initial Radioactive Nuclei')
         plt.title('Radioactive Decay Chain Simulation')
         plt.grid(True)
-        plt.savefig('radioactive_decay_chain_simulation.png')
+        plt.savefig('/home/ines/simulacion_de_sistemas_biologicos/out/3-ejercicio/radioactive_decay_chain_simulation.png')
         plt.show()
         
     def plot_state_distributions(self):
         plt.figure(figsize=(10, 6))
         for state_index, state_values in enumerate(zip(*self.state_distribution_history)):
-            plt.plot(self.time_history, state_values, label=f'State {state_index}')
-        plt.plot(self.time_history, self.theoretical_N_p_history, '--', label='Theoretical N_p')
-        plt.plot(self.time_history, self.theoretical_N_h_history, '--', label='Theoretical N_h')
-        plt.xlabel('Time')
-        plt.ylabel('Number of Nuclei')
-        plt.title('Nuclei State Distribution Over Time')
+            plt.plot(self.time_history, state_values, label=f'Estado {state_index}')
+        plt.plot(self.time_history, self.theoretical_N_p_history, '--', label='Teórica estado 2')
+        plt.plot(self.time_history, self.theoretical_N_h_history, '--', label='Teórica estado 1')
+        plt.xlabel('Tiempo/s')
+        plt.ylabel('Número de núcleos')
+        plt.title('Distribución de estado de los núcleos en el tiempo')
         plt.legend()
         plt.grid(True)
-        plt.savefig('radioactive_decay_chain_simulation_distribution.png')
+        plt.savefig('/home/ines/simulacion_de_sistemas_biologicos/out/3-ejercicio/radioactive_decay_chain_simulation_distribution.png')
         plt.show()
 
 if __name__ == "__main__":
-    num_nuclei = 20
-    # decay_constants = [0.693 / 20.8, 0.693 / 10]
+    num_nuclei = 10000
     decay_constants = [0.693 / 5, 0.693 / 10]
     simulation = RadioactiveDecayChainSimulation(num_nuclei, decay_constants)
     simulation.run_simulation()
